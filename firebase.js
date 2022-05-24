d73b0186ad5dfd2ca11822c6d295370cd9e433bd
muqadasurooj17 <muqadasurooj123@gmail.com>
2022-05-24 18:26:44 +0500

    first commit

---
+++ firebase.js
@@ -0,0 +1,14 @@
+ var firebaseConfig = {
+    apiKey: "AIzaSyA5LiuiLLWzH0-HP3Di6UmUwI5vL1Q6mTU",
+    authDomain: "mytestproject-274a7.firebaseapp.com",
+    projectId: "mytestproject-274a7",
+    storageBucket: "mytestproject-274a7.appspot.com",
+    messagingSenderId: "439971705325",
+    appId: "1:439971705325:web:752bc6884997cc7ac5f7e4",
+    measurementId: "G-D681JGV32P"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+
